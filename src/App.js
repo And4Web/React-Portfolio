efac01eac00336ddaa7a2a1d04182680cd156d56
Anand Shukla <anands2807@gmail.com>
2021-11-24 21:58:53 +0530

    carousel added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import BannerSection from './components/sections/BannerSection'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <BannerSection></BannerSection>
     </div>
   );
 }
---
+++ src/components/sections/BannerSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+export default function BannerSection() {
+  return (
+    <div>
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+        </div>
+      </div>      
+    </div>
+  )
+}
