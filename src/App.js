d790f056ff12f4b4d74ad732fb6e80b307e443bc
Anand Shukla <anands2807@gmail.com>
2021-11-24 16:14:18 +0530

    changes in header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>My First React Portfolio...</h1>
+      <Header/>
     </div>
   );
 }
