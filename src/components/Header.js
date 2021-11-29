bd7867f08247b5aef662fbbf2d9a98d0082f9d47
Anand Shukla <anands2807@gmail.com>
2021-11-29 11:02:09 +0530

    router wrapped.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,23 @@ function Header() {
         <div className="container-fluid">
           <a className="navbar-brand text-light" href="/">And4Web</a>
             <div style={{float:'right'}}>
-            <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end"  >
-                <li className="nav-item">
-                  <Link to='/' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-home"/>Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/skills' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-tools"/>Skills
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/projects' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-tasks"/>Projects
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/contact' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-phone"/>Contact
-                  </Link>
-                </li>
+              <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end"  >
+                  <li className="nav-item">
+                    <Link to='/' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-home"/>Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/skills' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-tools"/>Skills
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/projects' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-tasks"/>Projects
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/contact' className="nav-link active text-light ms-3 me-3" aria-current="page" ><i className="fas fa-phone"/>Contact
+                    </Link>
+                  </li>
               </ul>
             </div>
              
