01267ac4c3338d978dcc418fd587f22cc2ffb1b9
Anand Shukla <anands2807@gmail.com>
2021-11-29 21:15:36 +0530

    css changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,21 +9,21 @@ function Header() {
         <div className="container-fluid">
           <a className="navbar-brand text-light text-dark logo" href="/">And4Web</a>
             <div style={{float:'right'}}>
-              <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end"  >
-                  <li className="nav-item">
-                    <Link to='/' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-home me-2"/>Home
+              <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end menubar"  >
+                  <li className="nav-item menu-item">
+                    <Link to='/' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-home me-2 menu-icon"/>Home
                     </Link>
                   </li>
-                  <li className="nav-item">
-                    <Link to='/skills' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-tools me-2"/>Skills
+                  <li className="nav-item menu-item">
+                    <Link to='/skills' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-tools me-2 menu-icon"/>Skills
                     </Link>
                   </li>
-                  <li className="nav-item">
-                    <Link to='/projects' className="nav-link active text-light ms-3 me-3 text-dark" ><i className="fas fa-tasks me-2"/>Projects
+                  <li className="nav-item menu-item">
+                    <Link to='/projects' className="nav-link active text-light ms-3 me-3 text-dark menu" ><i className="fas fa-tasks me-2 menu-icon"/>Projects
                     </Link>
                   </li>
-                  <li className="nav-item">
-                    <Link to='/contact' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-phone me-2"/>Contact
+                  <li className="nav-item menu-item">
+                    <Link to='/contact' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-phone me-2 menu-icon"/>Contact
                     </Link>
                   </li>
               </ul>
