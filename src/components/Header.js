48c18f6d5822eca2604df0c0304d67fd8b9ab58c
Anand Shukla <anands2807@gmail.com>
2021-11-29 21:11:10 +0530

    css changed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="header">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-          <a className="navbar-brand text-light" href="/">And4Web</a>
+          <a className="navbar-brand text-light text-dark logo" href="/">And4Web</a>
             <div style={{float:'right'}}>
               <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end"  >
                   <li className="nav-item">
-                    <Link to='/' className="nav-link active text-light ms-3 me-3"><i className="fas fa-home me-2"/>Home
+                    <Link to='/' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-home me-2"/>Home
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to='/skills' className="nav-link active text-light ms-3 me-3"><i className="fas fa-tools me-2"/>Skills
+                    <Link to='/skills' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-tools me-2"/>Skills
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to='/projects' className="nav-link active text-light ms-3 me-3" ><i className="fas fa-tasks me-2"/>Projects
+                    <Link to='/projects' className="nav-link active text-light ms-3 me-3 text-dark" ><i className="fas fa-tasks me-2"/>Projects
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to='/contact' className="nav-link active text-light ms-3 me-3"><i className="fas fa-phone me-2"/>Contact
+                    <Link to='/contact' className="nav-link active text-light ms-3 me-3 text-dark"><i className="fas fa-phone me-2"/>Contact
                     </Link>
                   </li>
               </ul>
