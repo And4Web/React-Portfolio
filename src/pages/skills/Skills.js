e8a03ba0d4c40ac06ab4972c03f55badcb520ada
And4Web <anands2807@gmail.com>
2022-02-04 11:15:14 -0800

    skills section done.

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Skills.css";
 import skills from "../../img/skills.png";
+import mongo from "../../img/mongo.png";
+import react from "../../img/react.png";
+import node from "../../img/node.png";
+import express from "../../img/express.png";
+import js from "../../img/js.png";
+import html from "../../img/html.png";
+import css from "../../img/css.png";
+import sass from "../../img/sass.png";
+import bs from "../../img/bs.png";
+import rnative from "../../img/rnative.png";
+import postgre from "../../img/postgre.png";
+import next from "../../img/next.png";
 
 export default function Home() {
   return (
@@ -9,11 +21,47 @@ export default function Home() {
         <div className="sk-left-wrapper">
           <h1 className="display-1">Skills</h1>
           <div className="sk-desc">
-            <p>these are my skills.... work going on...</p>
-            <p>something here...</p>
+            <div className="sk-logos">
+              <div className="sk-logos-card">
+                <img src={html} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={css} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={js} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={sass} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={bs} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={react} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={next} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={rnative} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={node} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={express} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={mongo} alt="" className="sk-logo" />
+              </div>
+              <div className="sk-logos-card">
+                <img src={postgre} alt="" className="sk-logo" />
+              </div>
+            </div>
           </div>
+          <button className="btn btn-secondary sk-btn">Skills</button>
         </div>
-        <button className="btn btn-secondary sk-btn">Skills</button>
       </div>
       <div className="sk-right">
         <div className="sk-bg"></div>
