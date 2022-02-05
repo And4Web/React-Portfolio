a171e8a7220164f314539dc1356182bb594d8f10
And4Web <anands2807@gmail.com>
2022-02-05 12:41:35 -0800

    projects page created project cards created.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Intro from "../pages/intro/Intro";
 import About from "../pages/about/About";
 import Skills from "../pages/skills/Skills";
 import Projects from "../pages/projects/Projects";
+import Contact from "../pages/contact/Contact";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -6,11 +6,38 @@ export default function Home() {
     <div className="projects">
       <div className="pr-left">
         <div className="pr-left-wrapper">
-          <h1 className="display-1">Projects</h1>
+          <h1 className="display-1 pr-title">Projects</h1>
+          <div className="pr-cards">
+            <div className="pr-card">
+              <h3>Project1</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project2</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project3</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project4</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project5</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project6</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project7</h3>
+            </div>
+            <div className="pr-card">
+              <h3>Project8</h3>
+            </div>
+          </div>
         </div>
       </div>
       <div className="pr-right">
         <div className="pr-bg"></div>
+        <button className="btn btn-secondary pr-btn">Explore Projects</button>
       </div>
     </div>
   );
