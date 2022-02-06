9fc776e4fe5f6f45bfd8ed77b82adca901b1580b
And4Web <anands2807@gmail.com>
2022-02-06 13:53:38 -0800

    contact left section done

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "./Contact.css";
-import contactbg from "../../img/contactbg.png";
+import phone from "../../img/phone.png";
+import email from "../../img/email.png";
 
 export default function Home() {
   return (
     <div className="contact">
       <div className="co-wrapper">
         <div className="co-left">
-          <h1>left</h1>
+          <div className="co-bg"></div>
+          <div className="co-info-wrapper">
+            <h1 className="display-1 co-title">Let's discuss</h1>
+            <div className="co-info">
+              <div className="co-info-phone">
+                <img src={phone} alt="" className="co-info-img" />
+                <p className="info-data">+91 8787276894</p>
+              </div>
+              <div className="co-info-email">
+                <img src={email} alt="" className="co-info-img" />
+                <p className="info-data">andpmedia1@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="co-right">
           <h1>right</h1>
