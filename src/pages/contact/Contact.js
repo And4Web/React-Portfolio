0e69bd9602e1ce74e0f6199cccce6f55beacbd38
And4Web <anands2807@gmail.com>
2022-02-06 14:53:11 -0800

    contact form structure done.

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
 import phone from "../../img/phone.png";
 import email from "../../img/email.png";
@@ -8,6 +8,11 @@ import twitter from "../../img/twitter.png";
 import facebook from "../../img/facebook.png";
 
 export default function Home() {
+  const formRef = useRef();
+  const formSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="contact">
       <div className="co-wrapper">
@@ -32,11 +37,21 @@ export default function Home() {
               </p>
 
               <div className="social">
-                <img src={email} alt="" className="co-info-img" />
-                <img src={linkedin} alt="" className="co-info-img"></img>
-                <img src={github} alt="" className="co-info-img"></img>
-                <img src={twitter} alt="" className="co-info-img"></img>
-                <img src={facebook} alt="" className="co-info-img"></img>
+                <a href="" target="blank">
+                  <img src={email} alt="" className="co-info-img" />
+                </a>
+                <a href="https://www.linkedin.com/in/and4web/" target="blank">
+                  <img src={linkedin} alt="" className="co-info-img" />
+                </a>
+                <a href="https://github.com/And4Web" target="blank">
+                  <img src={github} alt="" className="co-info-img" />
+                </a>
+                <a href="https://twitter.com/And4Web" target="blank">
+                  <img src={twitter} alt="" className="co-info-img" />
+                </a>
+                <a href="https://www.facebook.com/And4Web/" target="blank">
+                  <img src={facebook} alt="" className="co-info-img" />
+                </a>
               </div>
             </div>
           </div>
@@ -47,16 +62,16 @@ export default function Home() {
             <h3>What's your Project? </h3>
             <p>
               Let's talk about it and see how to work on it. You can hire me as
-              yout Web/Mobile App Development expert. Message me here:
+              your Web/Mobile App Developer. Message me here:
             </p>
           </div>
 
-          <form>
+          <form ref={formRef} onSubmit={formSubmit}>
             <input type="text" placeholder="Your name" name="user_name" />
             <input type="text" placeholder="Your email" name="use_email" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <textarea rows="6" placeholder="Your message" />
-            <button>Submit</button>
+            <button className="btn btn-secondary co-btn">Submit</button>
           </form>
         </div>
       </div>
