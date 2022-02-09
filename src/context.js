d9ddc18ed6ac67b787f8a4cee9f303245ebd5cec
And4Web <anands2807@gmail.com>
2022-02-09 09:42:40 -0800

    some data regarding projects added to context.js

--- src/context.js
+++ src/context.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
 
-export default class context extends Component {
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        title: "E-commerce App using React JS",
+        img: "",
+        desc: "",
+        techUsed: [],
+      },
+      {
+        id: 2,
+        title: "Contact Manager App using React JS",
+        img: "",
+        desc: "",
+        techUsed: [],
+      },
+      {
+        id: 3,
+        title: "Food Ordering App using React Native",
+        img: "",
+        desc: "",
+        techUsed: [],
+      },
+    ],
+  };
+
   render() {
     return <div></div>;
   }
 }
+
+export const Consumer = Context.Consumer;
