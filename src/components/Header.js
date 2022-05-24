151ca7d4552f2cf59cee28e0c43903578f466141
And4Web <anands2807@gmail.com>
2022-05-24 08:53:24 +0530

    header added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,32 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand text-light text-dark logo" href="/">And4Web</a>
-            <div style={{float:'right'}}>
-              <ul className="nav me-auto mb-2 mb-lg-0 justify-content-end menubar"  >
-                  <li className="nav-item menu-item">
-                    <Link to='/' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-home me-2 menu-icon"/>Home
-                    </Link>
-                  </li>
-                  <li className="nav-item menu-item">
-                    <Link to='/skills' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-tools me-2 menu-icon"/>Skills
-                    </Link>
-                  </li>
-                  <li className="nav-item menu-item">
-                    <Link to='/projects' className="nav-link active text-light ms-3 me-3 text-dark menu" ><i className="fas fa-tasks me-2 menu-icon"/>Projects
-                    </Link>
-                  </li>
-                  <li className="nav-item menu-item">
-                    <Link to='/contact' className="nav-link active text-light ms-3 me-3 text-dark menu"><i className="fas fa-phone me-2 menu-icon"/>Contact
-                    </Link>
-                  </li>
-              </ul>
-            </div>
-             
-        </div>         
+      <nav className="navbar">
+        <div className="container">
+          <Link to="/" className="brand-logo" href="/">
+            And4Web
+          </Link>
+          <div className="menubar">
+            <Link to="/home" className="menu-item">
+              Home
+            </Link>
+            <Link to="/skills" className="menu-item">
+              Skills
+            </Link>
+            <Link to="/projects" className="menu-item">
+              Projects
+            </Link>
+            <Link to="/contact" className="menu-item">
+              Contact
+            </Link>
+          </div>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 export default Header;
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,12 @@ import About from "../pages/about/About";
 import Skills from "../pages/skills/Skills";
 import Projects from "../pages/projects/Projects";
 import Contact from "../pages/contact/Contact";
+import Header from "../components/Header";
 
 function Home() {
   return (
     <div>
+      <Header />
       <Intro />
       <About />
       <Skills />
